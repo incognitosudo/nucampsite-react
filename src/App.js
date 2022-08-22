41bcce4c48280cb674af8321ff4ed8628bf153ec
Incognito-Burrito <gfranco.mg@gmail.com>
2022-08-21 22:41:41 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
+//import React from 'react';
 //import logo from './logo.svg';
 //import { Counter } from './features/counter/Counter';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
+//import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
-import CampsiteCard from './features/campsites/CampsiteCard.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
+import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+//import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
-      <CampsiteCard campsite={CAMPSITES[0]} />
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }

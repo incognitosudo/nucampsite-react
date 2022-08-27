ef27c1bf82f273de086bc6f2a7953e5bc278888b
Incognito-Burrito <gfranco.mg@gmail.com>
2022-08-27 07:13:50 -0700

    added arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,60 @@
+const things = [
+    {
+        id: 0,
+        title: 'whiskers on kittens',
+        favorite: true,
+        points: 97
+    },
+    {
+        id: 1,
+        title: 'raindrops on roses',
+        favorite: true,
+        points: 77
+    },
+    {
+        id: 2,
+        title: 'brown paper packages tied up with string',
+        favorite: true,
+        points: 87
+    },
+    {
+        id: 3,
+        title: 'dog bite',
+        favorite: false,
+        featured: 'true',
+        points: 12
+    },
+    {
+        id: 4,
+        title: 'bee sting',
+        favorite: false,
+        points: 6
+    }
+];
+//Array.filter() is fixed so what shows is still the same
+//Array.map() changes so if you want uppercase you can.
+const names = ['anne', 'barry', 'chloe'];
+const bigNames = names.map((name) => name.toUpperCase());
+
+//Array.filter(callbackFn) and Array.find(callbackFn);
+const myFavoriteThings = things.filter((thing) => thing.favorite); //assign variable myFavoriteThings to return value from things.filter. This array only returns items with properties of true
+const thingsWithOverFiftyPoints = things.filter((thing) => thing.points > 50);
+
+//Array.find() takes a testing funciton and returns only returns first item that passes. Like matching ID.
+const selectThingByID = (id) => {
+return things.find((thing) => thing.id ===id);
+}
+const selectedThing = selectThingByID(3);
+
+//this function usings Array.find() to find the first element in the array with "feautured" as property.
+const feauturedThing = things.find((thing) => thing.feautured);
+
+
+console.log(names);
+console.log(bigNames);
+console.log(myFavoriteThings);
+console.log(things);
+console.log(thingsWithOverFiftyPoints);
+console.log(selectedThing);
+console.log(feauturedThing);
+//or console.log(things.filter((thing) => thing.feautred[0]); but more code.
--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,20 +1,23 @@
 import { Card, CardImg, CardImgOverlay, CardTitle }from 'reactstrap';
 
 //function component named CampsiteCard with parameter props
-const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
     return (
         //renders Card component
         <Card>
             <CardImg 
                 width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
+                src={image}
+                alt={name}
             /> 
             <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
+                <CardTitle>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
     );
 }
 
 export default CampsiteCard;
+
+

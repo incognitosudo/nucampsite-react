058652b42cafcb25917ccc8810dc36d46b47ab29
Incognito-Burrito <gfranco.mg@gmail.com>
2022-08-28 16:50:11 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@
 //import { Container, Navbar, NavbarBrand } from 'reactstrap';
 //import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+//import CampsitesList from './features/campsites/CampsitesList';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 //import { CAMPSITES } from './app/shared/CAMPSITES';
+import { Route, Routes} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 
+//route path with prop of 'directory'
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
